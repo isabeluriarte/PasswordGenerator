ab759a4eb9766404aeb31cff256ec7d627faaee0
Isabel Uriarte <uriarte.isabel@gmail.com>
2019-12-03 18:03:22 -0800

    updates to js

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,42 @@ var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
 
 //this function will fire when you click the generate password button on the page.  I've set it to alert "You've clicked a button" and return a password of password for now. Update it to make your password
+
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upper = lower.map(function (x) { return x.toUpperCase() })
+var passwordArr = []
+
+alert("Welcome to the World's Best Password Generator! \nYou can customize your password by determining how long you would like it to be, and whether or not you would like to include special characters, numbers, lowercase letters, and uppecase letters.")
+
 function generatePassword() {
     //YOUR CODE HERE
+    var length = prompt("How long would you like your password to be? \n(Length must be between 8 and 128)")
+    if (length > 8 && length < 128) {
+        console.log(length)
+
+        if (confirm("Would you like to use special characters?"))
+            passwordArr.push(...special)
+
+        if (confirm("Would you like to use numbers?"))
+            passwordArr.push(...number)
+
+        if (confirm("Would you like to use lowercase letters?"))
+            passwordArr.push(...lower)
+
+        if (confirm("Would you like to use uppercase letters?"))
+            passwordArr.push(...upper)
+
+            console.log(passwordArr)
+    }
+
+
     alert("ive been clicked");
     return "password";
 }
+
+generatePassword()
 
 // Write password to the #password input
 function writePassword() {
